d38e17967eaf98a31758e27986e549617d908ba6
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-15 20:40:40 +0600

    daisy UI + json created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-   const [count, setCount] = useState(0);
-
    return (
       <>
          <h1 className="text-3xl text-red-400">Vite + React</h1>
+         <button className="btn btn-active btn-neutral">Neutral</button>
       </>
    );
 }
