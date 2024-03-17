2899b21240f2dd302071dfb570a9cad5e4f42247
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-17 22:22:19 +0600

    navbar item renamed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,8 +32,9 @@ const Navbar = () => {
                   className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
                >
                   <li>
-                     <a>Item 1</a>
+                     <a>Home</a>
                   </li>
+
                   <li>
                      <a>Recipes</a>
                   </li>
